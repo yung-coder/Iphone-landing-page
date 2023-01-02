8853af5a9e4626b7c80b854d70e18edf491ecb87
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-02 14:12:53 +0530

    cards adding

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,9 +1,24 @@
-'use client';
+"use client";
 
-const ExploreCard = () => (
-  <div className="text-black">
-    Explore Card
-  </div>
+import { motion, Variants } from "framer-motion";
+
+import styles from "../styles";
+
+import { fadeIn } from "../utils/motion";
+
+const ExploreCard = ({ id, imgUrl, title, index, active, handelClick }) => (
+  <motion.div
+    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  >
+    <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover rounded-[24px]"
+    />
+  </motion.div>
 );
 
 export default ExploreCard;
