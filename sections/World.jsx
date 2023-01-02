c227a23450036e3f43a286c958773f9016fc6ac7
yung-coder <chandel.sanjeev5@gmail.com>
2023-01-02 16:20:30 +0530

    world map

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,51 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+import { TitleText, TypingText } from "../components";
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: "false", amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex  flex-col `}
+    >
+      <TypingText title="Our Users" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            We will acknowledge the receipt of your complaint and, if
+            appropriate, provide you with a reference number to track your
+            complaint.
+          </>
+        }
+        textStyles="text-center"
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[60px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="people-03.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
